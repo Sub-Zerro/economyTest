1ff05739e0fff6389c15bc09db97e6a99d7f5d8c
boss_of_the_GYM <jimi4kuryki@gmail.com>
2023-07-29 12:06:59 +0300

    new_version_with_new_fetch2

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-const express = require("express");
-
-const app = express();
-
-let answerArr = [];
-
-const urlencodedParser = express.urlencoded({extended: false});
-// app.get("/", function (request, response) {
-//     response.sendFile(__dirname + "/word2.txt");
-// });
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const pg = require('pg');
-//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
-
 const {Pool, Client} = require('pg');
-
+const filepath = path.join(__dirname, 'index.html');
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
 const pool = new Pool({
     host: 'db.lsqzvzhcwzpvqhgpdwbl.supabase.co',
     // Do not hard code your username and password.
@@ -27,37 +15,153 @@ const pool = new Pool({
     port: 5432,
     ssl: true
 })
+pool.connect();
+
+let arr = [];
+const ransw = '143422312222322441';
+const ranswArr = ['1', '4', '3', '4', '2', '2', '3', '1', '2', '2', '2', '2', '3', '2', '2', '4', '4', '1'];
+//const client = new pg.Client(pool);
+
+http.createServer(function(request, response){
+    if (request.url==='/'){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        var myReadStream = fs.createReadStream(filepath, 'utf8');
+        myReadStream.pipe(response);
+    }
 
 
-// асинхронное чтение
-let indexData;
-app.get("/file", function (req, res){
-    // fs.readFile("word3.txt", "utf8",
-    //     function(error,data){
-    //         console.log("Асинхронное чтение файла");
-    //         if(error) throw error; // если возникла ошибка
-    //         console.log(data);  // выводим считанные данные
-    //         indexData = data;
-    // });
-    indexData = fs.readFileSync('word3.txt').toString();
-    res.sendFile(__dirname + "/index.html");
-})
+    if (whatUrl(request.url) === '/about?'){
+        post_to_base(request.url);
+    }
 
-app.get("/", function (req, res){
-    res.sendFile(__dirname + "/form.html");
-})
+    if (request.url=='/pups227'){
+        (async ()=>{
+            await queryDatabase();
+            //await (()=>{
+            await response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+                // let str = '';
+                // for (let i = 0; i<arr.length; i++){
+                //     str+=`${arr[i][0]} - ${arr[i][3]} баллов, ${100*arr[i][3]/18} %`;
+                //     str += ' \n ';
+                // }
+                // response.end(str);
+                for (let i = 0; i<arr.length; i++){
 
-app.post("/", urlencodedParser, function (request, response) {
-    //if(!request.body) return response.sendStatus(400);
+                    response.write(`<div>${arr[i][0]} - ${arr[i][3]} баллов из 18, ${100*arr[i][3]/18} % </div>`);
+                }
+            //})
+        })()
+    }
 
+
+
+}).listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Функции
+function whatUrl(url){
+    let str = "";
+    if (url.length>=6){
+        for (let i = 0; i < 7; i++){
+            let symb = url[i];
+            str+=`${symb}`;
+        }
+    }
+    return str;
+}
+
+function post_to_base(url){
+    let current_url = new URL(`http://localhost:3000${url}`);
+    let search_params = current_url.searchParams;
+
+    let name = search_params.get('name');
+    let answer = search_params.get('scope');
+
+    console.log('name = ', name);
+    console.log('answer = ', answer);
+
+    const query = `INSERT INTO results(name, answer)values('${name}', '${answer}');`;
 
 
     pool.query(`
-            INSERT INTO results(name, answer)values('${request.body.name}', '${request.body.numberOfQ}')
+            INSERT INTO results(name, answer)values('${name}', '${answer}');
 
     `, (err, res) => {
         console.log(err, res);
+        //pool.end();
     })
-});
+    //return 0;
+}
 
-app.listen(3000, () => console.log("Сервер запущен..."));
+
+
+async function queryDatabase() {
+
+    //console.log(`Running query to PostgreSQL server: ${pool.host}`);
+
+    const query = "select name,answer from results;";
+
+    await pool.query(query)
+        .then(res => {
+            const rows = res.rows;
+
+            rows.map(row => {
+                console.log(`Read: ${JSON.stringify(row)}`);
+            });
+
+            for (let i = 0; i<res.rows.length; i++){
+                arr.push([res.rows[i]['name'],res.rows[i]['answer']]);
+            }
+
+            console.log(arr);
+
+            //process.exit();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+
+        for(let i = 0; i<arr.length; i++){
+            arr[i].push(arr[i][1].split(''))
+        }
+        console.log(arr)
+
+        for(let i = 0; i<arr.length; i++){
+            let counter = 0;
+            for(let k = 0; k<arr[i][2].length; k++){
+                if (arr[i][2][k]==ranswArr[k]){
+                    counter++;
+                }
+
+            }
+            arr[i].push(counter)
+        }
+        console.log(arr)
+
+
+
+}
+
+
+
+
+
+
+
+
